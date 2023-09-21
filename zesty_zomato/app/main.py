c92f8683a9e6b4098a7a73551af3058a09563ee1
AnshKathpal <storage.2.3107@gmail.com>
2023-09-22 01:16:48 +0530

    CRUD

--- zesty_zomato/app/main.py
+++ zesty_zomato/app/main.py
@@ -1,11 +1,78 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-menu = []
+menu = [
+    {
+        "dishId": 1,
+        "dish_name": "Spaghetti Carbonara",
+        "price": 12.99,
+        "availability": True
+    },
+    {
+        "dishId": 2,
+        "dish_name": "Margherita Pizza",
+        "price": 10.49,
+        "availability": True
+    },
+    {
+        "dishId": 3,
+        "dish_name": "Chicken Alfredo",
+        "price": 14.99,
+        "availability": True
+    },
+    {
+        "dishId": 4,
+        "dish_name": "Caesar Salad",
+        "price": 8.99,
+        "availability": True
+    },
+    {
+        "dishId": 5,
+        "dish_name": "Hamburger",
+        "price": 9.99,
+        "availability": True
+    },
+    {
+        "dishId": 6,
+        "dish_name": "Sushi Platter",
+        "price": 22.99,
+        "availability": True
+    },
+    {
+        "dishId": 7,
+        "dish_name": "Veggie Wrap",
+        "price": 7.99,
+        "availability": True
+    },
+    {
+        "dishId": 8,
+        "dish_name": "Chocolate Cake",
+        "price": 6.49,
+        "availability": True
+    },
+    {
+        "dishId": 9,
+        "dish_name": "Grilled Salmon",
+        "price": 16.99,
+        "availability": True
+    },
+    {
+        "dishId": 10,
+        "dish_name": "Fruit Salad",
+        "price": 5.99,
+        "availability": True
+    }
+]
+
+
+orders = []
+orderCounter = 1
+
 
 @app.route("/")
 def homepage():
-    return 'Welcome to Zesty Zomato!', 200
+    return jsonify(menu_data=menu), 200
+
 
 @app.route("/menu", methods=["POST"])
 def create_menu():
@@ -13,7 +80,8 @@ def create_menu():
     if "dishId" not in data or "dish_name" not in data or "price" not in data:
         return jsonify({"error": "Invalid JSON Data"}), 400
     menu.append(data)
-    return jsonify({'message': 'Menu item created'}), 200
+    return jsonify({'message': 'Menu item created'}), 201
+
 
 @app.route("/menu/<int:dishId>", methods=["PUT"])
 def update_menu(dishId):
@@ -21,11 +89,55 @@ def update_menu(dishId):
     for item in menu:
         if item["dishId"] == dishId:
             item.update(data)
-            return jsonify({'message' : "Menu Updated"}), 200
+            return jsonify({'message': "Menu Updated"}), 200
     return jsonify({'error': 'Menu item not found'}), 404
+
+
+@app.route("/menu/<int:dishId>", methods=["DELETE"])
+def delete_menu(dishId):
+    for item in menu:
+        if item["dishId"] == dishId:
+            menu.remove(item)
+            return jsonify({"message": "Item Deleted"}), 204
+    return jsonify({"error": "Item not found"}), 404
+
+
+@app.route("/orders", methods=["POST"])
+def order_placed():
+    data = request.json
+    customer_name = data.get("customer_name")
+    dish_ids = data.get("dish_ids")
+
+    if not customer_name or not dish_ids:
+        return jsonify({"message": "Invalid Order"}), 400
+
+    orderItems = []
+    totalPrice = 0
+
+    for id in dish_ids:
+        dish = next((item for item in menu if item["dishId"] == id), None)
+        if dish and dish["availability"]:
+            orderItems.append(dish)
+            totalPrice += dish["price"]
+        else:
+            return jsonify({"error": f"Dish with dishId {id} is unavailabile"}), 400
+
+    global orderCounter
+    order_id = orderCounter
+    orderCounter += 1
+
+    order = {
+        "order_id": order_id,
+        "customer_name": customer_name,
+        "order_items": orderItems,
+        "total_price": totalPrice,
+        "order_status": "received"
+    }
+
+    orders.append(order)
+    return jsonify({"message": f"Order with order_id {order_id} placed successfully"}), 201
+
 
 
 if __name__ == '__main__':
     app.run()
-
-
--- zesty_zomato/tests/test_main.py
+++ zesty_zomato/tests/test_main.py
@@ -6,14 +6,23 @@ class TestZestyZomatoApp(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_homepage(self):
+# test for get
+
+    def test_display_menu(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
+
+        response_data = response.get_json()
+        self.assertIn('menu_data', response_data)
+
+# test for get
+
+# Tests for post
 
     def test_create_menu_valid(self):
         response = self.app.post(
             "/menu", json={'dishId': 1, 'dish_name': "Pizza", 'price': 10.33, 'availability': True})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
 
     def test_create_menu_invalid(self):
 
@@ -22,22 +31,81 @@ class TestZestyZomatoApp(unittest.TestCase):
         )
         self.assertEqual(response.status_code, 400)
 
+# Tests for post
+
+# Tests for update
+
     def test_update_menu_valid(self):
         self.app.post(
-            "/menu", json={'dishId': 1, 'dish_name': "Pizza", 'price': 10.33, 'availability': True})
+            "/menu", json={'dishId': 11, 'dish_name': "Pizza", 'price': 10.33, 'availability': True})
         response = self.app.put(
-            "/menu/1", json={'dish_name': "New Pizza", 'price': 11.23, 'availability': False}
+            "/menu/11", json={'dish_name': "New Pizza", 'price': 11.23, 'availability': False}
         )
         self.assertEqual(response.status_code, 200)
 
-        updated_item = menu[0]
+        updated_item = next(item for item in menu if item["dishId"] == 11)
         self.assertEqual(updated_item["dish_name"], "New Pizza")
         self.assertEqual(updated_item["price"], 11.23)
         self.assertEqual(updated_item["availability"], False)
 
     def test_update_invalid_menu_item(self):
-        response = self.app.put("/menu/2", json={'dish_name': 'Updated Dish'})
+        response = self.app.put("/menu/99", json={'dish_name': 'Updated Dish'})
         self.assertEqual(response.status_code, 404)
+
+# Tests for update
+
+# Tests for delete
+
+    def test_delete_menu_item(self):
+        self.app.post(
+            "/menu", json={'dishId': 11, 'dish_name': "Pizza", 'price': 10.33, 'availability': True}
+        )
+        response = self.app.delete(
+            "/menu/11"
+        )
+        self.assertEqual(response.status_code, 204)
+
+    def test_delete_invalid_item(self):
+        response = self.app.delete("menu/99")
+        self.assertEqual(response.status_code, 404)
+
+# Tests for delete
+
+# Tests for  Order Placed
+
+    def test_order_placed(self):
+        response = self.app.post(
+            "/orders",
+            json={"customer_name": "Ansh", "dish_ids": [1, 2, 3]}
+        )
+        self.assertEqual(response.status_code, 201)
+
+    def test_order_placed_failed(self):
+        response = self.app.post(
+            "/orders",
+            json={"customer_name": "Ansh", "dish_ids": [12, 13, 14]}
+        )
+        self.assertEqual(response.status_code, 400)
+
+# Tests for  Order Placed
+
+# Tests for  Update Placed
+
+    def test_order_update(self):
+        response = self.app.post(
+            "/orders",
+            json={"customer_name": "Ansh", "dish_ids": [1, 2, 3]}
+        )
+
+        orderId = response.get_json().get("message").split()[-1]
+        response = self.app.put(
+            f"/orders/{orderId}",
+            json={"order_status": "prepared"}
+        )
+        self.assertEqual(response.status_code, 200)
+
+
+# Tests for  Update Placed
 
 
 if __name__ == '__main__':
